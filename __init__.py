09310112631a923d6e3f63fd6be0170dd6b33625
vernuexbuler <vernuexbuler@protonmail.com>
2026-03-22 08:28:26 +0100

    Initialize the numpy_ai_solver package and expose key classes

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Initialize the package and expose key classes.
+
+This module provides access to the key classes used in the numpy_ai_solver package.
+"""
+
+from .key_class_1 import KeyClass1
+from .key_class_2 import KeyClass2
+
+__all__ = ['KeyClass1', 'KeyClass2']
